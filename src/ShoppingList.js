e67954f0491e07ba7f3e5e6e83b2ca95fb380953
Ben Harter-Murphy <benhartermurphy@gmail.com>
2022-01-24 20:37:59 -0500

    added form and list

---
+++ src/ShoppingList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ShoppingListForm from './ShoppingListForm';
+
+
+class ShoppingList extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            items:[
+                {name: "Milk", qty: "2 gals"},
+                {name: "Bread", qty: "2 loaves"}
+            ]
+        }
+    }
+    renderItems() {
+        return(
+            <ul>
+                {this.state.items.map(item => (
+                    <li>
+                        {item.name} - {item.qty}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+    render(){
+        return(
+            <div>
+                <h1>Shopping List</h1>
+                {this.renderItems()}
+                <ShoppingListForm/>
+            </div>
+
+        )
+    }
+}
+export default ShoppingList
---
+++ src/ShoppingListForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class ShoppingListForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {name: '', qty: ''}
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    render(){
+        return(
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input 
+                    id="name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="qty">Quantity: </label>
+                <input 
+                    id="qty"
+                    name="qty"
+                    value={this.state.qty}
+                    onChange={this.handleChange}
+                />
+            </form>
+        )
+    }
+}
+export default ShoppingListForm
